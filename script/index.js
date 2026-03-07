4e5fe6619dd8f97bd5c4110387eff410aa95a3a4
mdsobujhossen <itmdsobuj@gmail.com>
2026-03-07 19:46:45 +0600

    login pages done

---
+++ script/index.js
@@ -0,0 +1,14 @@
+
+
+const logIn = () => {
+    const userNameInput = document.getElementById("userName-input")
+    const userName = userNameInput.value
+    const passwordInput = document.getElementById("password-input")
+    const password = passwordInput.value
+    if(userName === "admin" && password === "admin123"){
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("please enter correct credential")
+    }
+}
